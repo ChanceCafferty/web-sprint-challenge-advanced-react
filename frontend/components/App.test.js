5c82eab09e2971cc7e0a71a453ca3a181d7bf266
ChanceCafferty <chance.r.cafferty@gmail.com>
2022-05-19 01:26:04 -0500

    mvp check

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,9 +1,8 @@
-import server from './backend/mock-server'
+
 import React from 'react'
-import AppFunctional from './frontend/components/AppFunctional'
 import AppClass from './frontend/components/AppClass'
-import { render, fireEvent, screen } from '@testing-library/react'
-import userEvent from '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/react'
+// import userEvent from '@testing-library/jest-dom/extend-expect'
 
 
 // Test that the visible texts in headings, buttons, links... render on the screen.
